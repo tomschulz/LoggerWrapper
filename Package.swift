49df84123c65fd4b3b94e865323d8c9f87d36ccd
Tom Schulz <tom@silverorange.com>
2025-04-03 16:46:28 -0700

    Added Package.swift
    This repository is old. I added a package.swift, whihc I generated with ChatGPT.

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// Package.swift
+
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "LoggerWrapper",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(name: "LoggerWrapper", targets: ["LoggerWrapper"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.8.0")
+    ],
+    targets: [
+        .target(
+            name: "LoggerWrapper",
+            dependencies: [
+                .product(name: "CocoaLumberjackSwift", package: "CocoaLumberjack")
+            ],
+            path: "LoggerWrapper/LoggerWrapper",
+            exclude: ["Info.plist"]
+        )
+    ]
+)
+
